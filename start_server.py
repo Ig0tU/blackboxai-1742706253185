fcfb4423ef98632354e6a07fa64fabff0235964a
Ig0tU <124455599+Ig0tU@users.noreply.github.com>
2025-03-23 01:04:15 -0400

    Add start_server.py 1742706254909

---
+++ start_server.py
@@ -0,0 +1,8 @@
+from poe_api_wrapper import PoeServer
+tokens = [
+    {
+        "p-b": "PLACEHOLDER",
+        "p-lat": "PLACEHOLDER"
+    }
+]
+PoeServer(tokens=tokens, address="127.0.0.1", port="8000")
